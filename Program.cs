0aebbe4cb96d178996fc890bbba34932e2661b1d
mganji81 <116972342+mganji81@users.noreply.github.com>
2022-10-29 22:31:56 +0330

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool content = true;
+            bool nocontent = false;
+            Console.WriteLine("text is{0}", content);
+            Console.WriteLine("text is{0}", nocontent);
+
+        }
+    }
+}
